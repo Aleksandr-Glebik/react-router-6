22ec09d521a87050019d647dad118517fa4f5d54
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-21 16:10:39 +0300

    feat: add comp SinglePage and dynamic path for link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import BlogPage from './pages/BlogPage'
+import SinglePage from './pages/SinglePage'
 import NoFoundPage from './pages/NoFoundPage'
 import Layout from './components/Layout'
 
@@ -13,6 +14,7 @@ function App() {
           <Route index element={<HomePage />}/>
           <Route path='about' element={<AboutPage />}/>
           <Route path='posts' element={<BlogPage />}/>
+          <Route path='posts/:id' element={<SinglePage />}/>
           <Route path='*' element={<NoFoundPage />}/>
         </Route>
       </Routes>
