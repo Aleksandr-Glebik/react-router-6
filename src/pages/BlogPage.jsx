8ce7e0164cab730b517b3c3d9b0fddcb5e8f6760
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-21 22:03:21 +0300

    feat: add useSearchParams for BlogPage, create BlogFilter

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,8 +1,16 @@
 import React,{useState, useEffect}  from "react"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
+import BlogFilter from "../components/BlogFilter"
 
 const BlogPage = () => {
     const [posts, setPosts] = useState([])
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const postQuery = searchParams.get('post') || ''
+    //url.by /posts?post=23&data=sashaFront
+    const latest = searchParams.has('latest')
+
+    const startsFrom = latest ? 80 : 1
 
     useEffect( () => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -13,14 +21,24 @@ const BlogPage = () => {
     return (
         <div>
             <h1>Our news</h1>
+
+            <BlogFilter postQuery={postQuery}
+                        latest={latest}
+                        setSearchParams={setSearchParams}
+            />
+
             <Link to={'/posts/new'}> Add new post</Link>
+
             {
-                posts.map( post => (
+                posts.filter(
+                    post => post.title.includes(postQuery.toLocaleLowerCase()) && post.id >= startsFrom
+                ).map( post => (
                     <Link key={post.id} to={`/posts/${post.id}`}>
                         <li>{post.title}</li>
                     </Link>
                 ))
             }
+            
         </div>
     )
 }
