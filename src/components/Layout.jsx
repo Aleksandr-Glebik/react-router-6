ce6b0049f126710136ba7d1f2352c441c4263029
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-21 15:03:22 +0300

    feat: add CustomLink

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,15 @@
 import React from "react"
-import { Outlet, NavLink } from "react-router-dom"
+import { Outlet } from "react-router-dom"
+import CustomLink from './CustomLink'
 
 const Layout = () => {
-    const setActive = ({isActive}) => isActive ? 'active-link' : ''
-    // const setActive = ({isActive}) => ({color: isActive ? 'var(--color-active)' : 'white'})
+
     return (
         <>
         <header>
-            {/* <NavLink to="/" style={setActive}>Home</NavLink>
-            <NavLink to="/posts" style={setActive}>Blog</NavLink>
-            <NavLink to="/about" style={setActive}>About</NavLink> */}
-            <NavLink to="/" className={setActive}>Home</NavLink>
-            <NavLink to="/posts" className={setActive}>Blog</NavLink>
-            <NavLink to="/about" className={setActive}>About</NavLink>
+            <CustomLink to="/" >Home</CustomLink>
+            <CustomLink to="/posts" >Blog</CustomLink>
+            <CustomLink to="/about" >About</CustomLink>
         </header>
         <main className="container">
             <Outlet />
